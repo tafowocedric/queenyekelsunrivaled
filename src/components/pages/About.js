6341d265719495a223d64a2e11d945a74116891d
Benjamin <ejben55@gmail.com>
2021-06-18 10:05:50 +0100

    About and Contact Page format built

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,233 @@ import '../../css/pages/About.css';
 const About = () => {
     return (
         <PublicContainer>
-            <div>About Us</div>
+            <div className="about">
+
+                {/* ABOUT PAGE BANNER */}
+                <div className="page-banner-area">
+                    <div className="page-banner-content">
+                        <h2>About</h2>
+                        <ul>
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                About
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                {/* SECTION ONE */}
+                <section className="who-we-are">
+                    <div className="container">
+                        <div className="who-we-are-container">
+                            <div className="who-we-are-content">
+                                <span>About</span>
+                                <h3>Learn About Our Work and Cultural Activities</h3>
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                                </p>
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                                <ul className="who-we-are-list">
+                                    <li>
+                                        <span><p>1</p></span>
+                                        <p>Homelike Environment</p>
+                                    </li>
+                                    <li>
+                                        <span><p>2</p></span>
+                                        <p>Quality Educators</p>
+                                    </li>
+                                    <li>
+                                        <span><p>3</p></span>
+                                        <p>Safety and Security</p>
+                                    </li>
+                                    <li>
+                                        <span><p>4</p></span>
+                                        <p>Play to Learn</p>
+                                    </li>
+                                </ul>
+                                <div className="who-we-are-btn default-btn">
+                                    <a href="" className="">Read More</a>
+                                </div>
+                            </div>
+
+                            <div className="who-we-are-image-wrap">
+                                <img src="" alt="Founder of Queen Yekel's Unrivaled" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* SECTION TWO */}
+                <section className="fun-facts-area">
+                    <div className="container">
+                        <div className="fun-fact-elements">
+                            <div className="single-fun-fact">
+                                <h3>400</h3>
+                                <p>Students</p>
+                            </div>
+                            <div className="single-fun-fact bg-1">
+                                <h3>25</h3>
+                                <p>Teachers</p>
+                            </div>
+                            <div className="single-fun-fact bg-2">
+                                <h3>400</h3>
+                                <p>Classroom</p>
+                            </div>
+                            <div className="single-fun-fact bg-3">
+                                <h3>A</h3>
+                                <p>School Bus</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* SECTION THREE */}
+                <section className="teacher-area">
+                    <div className="container">
+                        <div className="single-teacher-text-item">
+                            <span>Expert Teacher</span>
+                            <h3>Our Online Instructor</h3>
+                            <p>
+                            Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                            <div className="teacher-btn default-btn">
+                                <a href="">Instructor Profile</a>
+                            </div>
+                        </div>
+
+                        <div className="single-teacher-item">
+                            <div className="overlay">
+                                <div className="image">
+                                    <img src="" alt="" />
+                                    <div className="content">
+                                        <h3>James Bond</h3>
+                                        <span>Music Teacher</span>
+                                        <ul className="social">
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-facebook"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-google-plus"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="single-teacher-item">
+                            <div className="overlay">
+                                <div className="image">
+                                    <img src="" alt="" />
+                                    <div className="content">
+                                        <h3>James Bond</h3>
+                                        <span>Music Teacher</span>
+                                        <ul className="social">
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-facebook"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-google-plus"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="single-teacher-item">
+                            <div className="overlay">
+                                <div className="image">
+                                    <img src="" alt="" />
+                                    <div className="content">
+                                        <h3>James Bond</h3>
+                                        <span>Music Teacher</span>
+                                        <ul className="social">
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-facebook"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-google-plus"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="single-teacher-item">
+                            <div className="overlay">
+                                <div className="image">
+                                    <img src="" alt="" />
+                                    <div className="content">
+                                        <h3>James Bond</h3>
+                                        <span>Music Teacher</span>
+                                        <ul className="social">
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-facebook"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-google-plus"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="single-teacher-item">
+                            <div className="overlay">
+                                <div className="image">
+                                    <img src="" alt="" />
+                                    <div className="content">
+                                        <h3>James Bond</h3>
+                                        <span>Music Teacher</span>
+                                        <ul className="social">
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-facebook"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="//#"><i className="bx bx1-google-plus"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* NEWSLETTER SECTION */}
+                
+                <div className="newsletter-area">
+                    <div className="container">
+                        <div className="newsletter-content">
+                            <h2>Do You Want To Know Get Update What’s Upcoming</h2>
+                        </div>
+                        <div className="newsletter-form-content">
+                            <form action="" className="newsletter-form">
+                                <input type="email" className="input-newsletter" name="EMAIL" id="" placeholder="Enter Email Address"/>
+                                <button type="submit">Subscribe Now</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </PublicContainer>
     );
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import PublicContainer from '../container/PublicContainer';
 
+
 const Home = () => {
     return (
         <PublicContainer>
-            <div className='layout-transparent'>
-                <div className='parallax-container'>
-                    <div className='parallax'>
-                        <div className='__title'>Learning Course Online</div>
-                        <div className='__sub-title'>
-                            We offer professional SEO services that help websites <span> score drastically in order to compete for the highest rankings.</span>
-                        </div>
-                    </div>
-                </div>
+            <div className="home">                
+
             </div>
         </PublicContainer>
     );

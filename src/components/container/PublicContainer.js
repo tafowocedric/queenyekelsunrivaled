d58e5daf6930adc760e5c343e5e5eef9216bc198
Vegas Cedric <vegascedric28@gmail.com>
2021-06-22 15:01:57 +0100

    enroll form designed

--- src/components/container/PublicContainer.js
+++ src/components/container/PublicContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PublicFooter from '../common/PublicFooter';
 import PublicHeader from '../common/PublicHeader';
+import ScrollToTop from '../common/ScrollToTop';
 
 const PublicContainer = ({ children, footer, footer_image }) => {
     return (
@@ -8,6 +9,7 @@ const PublicContainer = ({ children, footer, footer_image }) => {
             <PublicHeader />
             {children}
             {footer && <PublicFooter use_image={footer_image} />}
+            <ScrollToTop />
         </div>
     );
 };
---
+++ src/components/utils/Input.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const InputText = ({ name, label, className, ...rest }) => {
+    return (
+        <div className={`flex input ${className}`}>
+            {label && <label htmlFor={name}>{label}:</label>}
+            <input type='text' id={name} {...rest} />
+        </div>
+    );
+};
+
+const InputNumber = ({ name, label, ...rest }) => {
+    return (
+        <div className='flex input'>
+            {label && <label htmlFor={name}>{label}:</label>}
+            <input type='number' id={name} {...rest} />
+        </div>
+    );
+};
+
+const InputDate = ({ name, label, ...rest }) => {
+    return (
+        <div className='flex input'>
+            {label && <label htmlFor={name}>{label}:</label>}
+            <input type='date' id={name} {...rest} />
+        </div>
+    );
+};
+
+const InputRadio = ({ name, label, options, className, ...rest }) => {
+    return (
+        <div className={`flex input ${className}`}>
+            {label && <label htmlFor={name}>{label}:</label>}
+            <div className='radio-wrapper flex justify-space-evenly align-center mg-l-5'>
+                {options.map((option, i) => {
+                    return (
+                        <label className='flex align-center radio' key={i}>
+                            <input type='radio' name={name} {...rest} />
+                            <span className='flex align-center justify-center'>{option}</span>
+                        </label>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+const InputTextArea = ({ name, label, options, className, ...rest }) => {
+    return (
+        <div className={`flex input ${className}`}>
+            {label && <label htmlFor={name}>{label}:</label>}
+            <textarea name={name} id={name} {...rest}></textarea>
+        </div>
+    );
+};
+
+const Input = ({ type, ...rest }) => {
+    switch (type) {
+        case 'text':
+            return <InputText {...rest} />;
+
+        case 'number':
+            return <InputNumber {...rest} />;
+
+        case 'date':
+            return <InputDate {...rest} />;
+
+        case 'radio':
+            return <InputRadio {...rest} />;
+
+        case 'textarea':
+            return <InputTextArea {...rest}></InputTextArea>;
+
+        default:
+            break;
+    }
+};
+
+export default Input;

cb9a935b3c873fd76b0b9b58c3355c78c5f47ed1
Vegas Cedric <vegascedric28@gmail.com>
2021-07-07 12:14:43 +0100

    update from vegas in benjamin

--- server/routes/subscriber.js
+++ server/routes/subscriber.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { addSubscriber, getSubscriberById, getAllSubscribers } = require('../controller');
+const { addSubscriber, getSubscriberById, getAllSubscribers, forwardMail } = require('../controller');
 
 router.post('/add', addSubscriber);
 router.get('/:id', getSubscriberById);
 router.get('/', getAllSubscribers);
+router.post('/forwardmail', forwardMail);
 
 module.exports = router;
